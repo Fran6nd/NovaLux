aa4a7797b2050a6ff8715ea1e9392f3f166af2b1
François NOUAILLE <francoisnd1@gmail.com>
2020-02-10 11:15:07 +0100

    wip: tplink discovery

--- src/core/devices/devices/tplink.js
+++ src/core/devices/devices/tplink.js
@@ -25,5 +25,28 @@ class tplink extends baseclass {
 
 function discover() {
   console.log("hello");
+  let packet = tplink.encrypt('{"system":{"get_sysinfo":null}}');
+  var dgram = require("dgram");
+  var socket = dgram.createSocket("udp4");
+
+  socket.on("listening", function() {
+    socket.setBroadcast(true);
+    socket.send(packet, 0, packet.length, 9999, "255.255.255.255");
+    
+  });
+  socket.on("message", function (message, rinfo) {
+    console.log(tplink.decrypt(message));
+    });
+
+
+
+  socket.bind(8888);
+
+  let t = new Date().getTime();
+  while (new Date().getTime() - t < 4000){
+  }
+  socket.close();
+  //socket.close();
+
 }
 module.exports = discover;
