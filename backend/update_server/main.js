e27f4925a9f4f63826ab83a77a24a98187d8f439
darkassin <baptiste.roka@gmail.com>
2020-06-25 15:21:12 +0200

    test wifi and add req server

--- backend/update_server/main.js
+++ backend/update_server/main.js
@@ -1,35 +1,12 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const router = express.Router();
-const app = express();
-/* The DB module manage all SQL interactions and protect them. */
-const DB = require("./db.js").DB;
-/* Let's initialize the database. */
-DB.start();
+const express = require('express')
+const app = express()
 
-app.use(bodyParser.json());
+app.get('/update', function (req, res) {
+  res.send('Hello World!')
+  console.log('req get ')
+})
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(__dirname + "/rsc"));
 
-router.post("/public/newUser", (req, res) => {
-  if (DB.isEmailFree(req.body.email)) {
-    DB.addUser(req.body.email, req.body.password);
-    res.status(200);
-    res.end(JSON.stringify({ status: "Done" }));
-  } else {
-    res.status(400);
-    res.end(JSON.stringify({ status: "Failed" }));
-  }
-});
-
-router.get("*", function (req, res) {
-  console.log(JSON.stringify(req.body))
-  res.status(404).send("what???");
-});
-
-app.use(router);
-
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`App Started on PORT ${process.env.PORT || 3000}`);
-});
+app.listen(3000, function () {
+  console.log('server listening on port 3000!')
+})
--- embeded/src/network.py
+++ embeded/src/network.py
@@ -1,6 +1,9 @@
-import pywifi
-wifi = pywifi.PyWiFi()
-iface = wifi.interfaces()[0]
-
-
+import subprocess
+import time
 # pour l'instant casse les couilles
+#  nmcli device wifi list --> affiche les wifi
+p = subprocess.Popen(['nmcli device wifi list'], stdout=subprocess.PIPE, shell=True)
+#time.sleep(3)
+result = p.communicate()
+print(result)
+#print(p.stdout.read())
