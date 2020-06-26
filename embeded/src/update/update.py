aa90abfd4cc41ba491405aaa1b17c56866635108
darkassin <baptiste.roka@gmail.com>
2020-06-26 09:00:11 +0200

    back-end

--- embeded/src/update/update.py
+++ embeded/src/update/update.py
@@ -2,10 +2,22 @@ import requests
 import json
 
 
-resp = requests.get("http://localhost:3000/update")
-if resp.ok and resp.status_code == 200:   
-   print(resp.text)
-else:
-    print('error')
+
+def learnJSON():
+    with open('update.json') as json_data:
+        data_version = json.load(json_data)
+        #print(data_version)
+        # affiche la version du l'app
+        Version = data_version['version']
+        print(Version)
+        
+def reqUpdate():
+    header = "json à mettre"
+    resp = requests.get("http://localhost:3000/update")
+    if resp.ok and resp.status_code == 200:   
+        print(resp.text)
+    else:
+        print('error')
 
 
+learnJSON()
